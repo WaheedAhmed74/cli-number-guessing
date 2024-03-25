68c029ed634016e0c34e95a677b4a471e26896a3
WaheedAhmed74 <159169508+WaheedAhmed74@users.noreply.github.com>
2024-03-25 22:09:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randumNumber = Math.round(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([{
+        name: "userGuessNumber",
+        type: "number",
+        message: "Guess a number between 1-6"
+    }
+]);
+if (answers.userGuessNumber === randumNumber) {
+    console.log("Congratulation You choose a right number, and you win ");
+}
+else {
+    console.log("Sorry You choose a wrong number, Better luck next time ");
+}
